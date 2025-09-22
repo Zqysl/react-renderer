377b02fd703e49c99d9335497cd7e64a3de10935
Zack <zqy21@qq.com>
2025-09-22 15:32:20 +0800

    delete useless codes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
-
 import '@/libs/tailwindcss'
 import '@/libs/babel'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 
