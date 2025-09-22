5a029486a9ba61a8cfa7a48cde7953ab61611054
Zack <zqy21@qq.com>
2025-09-22 15:40:50 +0800

    replace CDN react with local version

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './ui';
 export * from './recharts';
+export * from './react';
---
+++ src/components/react/index.js
@@ -0,0 +1,4 @@
+export { jsx, jsxs, Fragment } from 'react/jsx-runtime';
+export { useState, useEffect, useRef, useMemo, useCallback } from 'react';
+export { createRoot } from 'react-dom/client';
+
